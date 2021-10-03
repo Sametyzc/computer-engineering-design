464dd308430651f32f04d0182d2d35e567ec2d9c
Sametyzc <sametyzc@hotmail.com>
2021-10-03 21:46:15 +0300

    flock algorithm added

---
+++ Baby Shark doo doo doo/Assets/Scripts/Boid.cs
@@ -0,0 +1,184 @@
+﻿using UnityEngine;
+
+public class Boid : MonoBehaviour
+{
+
+    BoidSettings settings;
+
+    // To update:
+    Vector3 acceleration;
+    [HideInInspector]
+    public Vector3 avgFlockHeading;
+    [HideInInspector]
+    public Vector3 avgAvoidanceHeading;
+    [HideInInspector]
+    public Vector3 centreOfFlockmates;
+    [HideInInspector]
+    public int numPerceivedFlockmates;
+
+    // Cached
+    Material material;
+    Transform cachedTransform;
+    Transform target;
+
+    // State
+    [HideInInspector]
+    public Vector3 position;
+    [HideInInspector]
+    public Vector3 forward;
+    Vector3 velocity;
+
+    float boundRadius;
+    static Vector3[] directions = null;
+    public bool showCollisionSphere;
+    void Awake()
+    {
+        material = transform.GetComponentInChildren<MeshRenderer>().material;
+        cachedTransform = transform;
+        InitRayCastDirections();
+    }
+
+    private void FixedUpdate()
+    {
+        position = transform.position;
+        if (showCollisionSphere)
+        {
+            if (directions != null)
+            {
+                for (int i = 0; i < directions.Length; i++)
+                {
+                    Vector3 dir = transform.TransformDirection(directions[i]);
+                    Ray ray = new Ray(position, dir);
+                    if (!Physics.SphereCast(ray, 0, boundRadius, settings.obstacleMask))
+                    {
+                        Debug.DrawLine(ray.origin, ray.origin + ray.direction, Color.w);
+                    }
+                    else
+                    {
+                        Debug.DrawLine(ray.origin, ray.origin + ray.direction, Color.red);
+                    }
+                }
+            }
+        }
+    }
+
+    public void Initialize(BoidSettings settings, bool showCollisionSphere)
+    {
+        target = null;
+        this.settings = settings;
+        this.showCollisionSphere = showCollisionSphere;
+        position = cachedTransform.position;
+        forward = cachedTransform.forward;
+
+        float startSpeed = (settings.minSpeed + settings.maxSpeed) / 2;
+        velocity = transform.forward * startSpeed;
+    }
+
+    public void UpdateBoid()
+    {
+        Vector3 acceleration = Vector3.zero;
+
+        if (target != null)
+        {
+            Vector3 offsetToTarget = (target.position - position);
+            acceleration = SteerTowards(offsetToTarget) * settings.targetWeight;
+        }
+
+        if (numPerceivedFlockmates != 0)
+        {
+            centreOfFlockmates /= numPerceivedFlockmates;
+
+            Vector3 offsetToFlockmatesCentre = (centreOfFlockmates - position);
+
+            var alignmentForce = SteerTowards(avgFlockHeading) * settings.alignWeight;
+            var cohesionForce = SteerTowards(offsetToFlockmatesCentre) * settings.cohesionWeight;
+            var seperationForce = SteerTowards(avgAvoidanceHeading) * settings.seperateWeight;
+
+            acceleration += alignmentForce;
+            acceleration += cohesionForce;
+            acceleration += seperationForce;
+        }
+
+        if (IsHeadingForCollision())
+        {
+            Vector3 collisionAvoidDir = ObstacleRays();
+            Vector3 collisionAvoidForce = SteerTowards(collisionAvoidDir) * settings.avoidCollisionWeight;
+            acceleration += collisionAvoidForce;
+        }
+
+        velocity += acceleration * Time.deltaTime;
+        float speed = velocity.magnitude;
+        Vector3 dir = velocity / speed;
+        speed = Mathf.Clamp(speed, settings.minSpeed, settings.maxSpeed);
+        velocity = dir * speed;
+
+        cachedTransform.position += velocity * Time.deltaTime;
+        cachedTransform.forward = dir;
+        position = cachedTransform.position;
+        forward = dir;
+    }
+
+    bool IsHeadingForCollision()
+    {
+        RaycastHit hit;
+        if (Physics.SphereCast(position, settings.boundsRadius, forward, out hit, settings.collisionAvoidDst, settings.obstacleMask))
+        {
+            return true;
+        }
+        else { }
+        return false;
+    }
+
+    Vector3 ObstacleRays()
+    {
+        Vector3[] rayDirections = BoidHelper.directions;
+
+        for (int i = 0; i < rayDirections.Length; i++)
+        {
+            Vector3 dir = cachedTransform.TransformDirection(rayDirections[i]);
+            Ray ray = new Ray(position, dir);
+            if (!Physics.SphereCast(ray, settings.boundsRadius, settings.collisionAvoidDst, settings.obstacleMask))
+            {
+                return dir;
+            }
+        }
+
+        return forward;
+    }
+
+    Vector3 SteerTowards(Vector3 vector)
+    {
+        Vector3 v = vector.normalized * settings.maxSpeed - velocity;
+        return Vector3.ClampMagnitude(v, settings.maxSteerForce);
+    }
+
+    public void InitRayCastDirections()
+    {
+        int numViewDirections = 100;
+        directions = new Vector3[numViewDirections];
+
+        float goldenRatio = (1 + Mathf.Sqrt(5)) / 2;
+        float angleIncrement = Mathf.PI * 2 * goldenRatio;
+
+        for (int i = 0; i < numViewDirections; i++)
+        {
+            float t = (float)i / numViewDirections;
+            float inclination = Mathf.Acos(1 - 2 * t);
+            float azimuth = angleIncrement * i;
+
+            float x = Mathf.Sin(inclination) * Mathf.Cos(azimuth);
+            float y = Mathf.Sin(inclination) * Mathf.Sin(azimuth);
+            float z = Mathf.Cos(inclination);
+            directions[i] = new Vector3(x, y, z);
+        }
+        boundRadius = Vector3.Distance(directions[0], position);
+    }
+
+    public void SetColour(Color col)
+    {
+        if (material != null)
+        {
+            material.color = col;
+        }
+    }
+}
